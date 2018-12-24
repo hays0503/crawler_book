ffb921a9b2972a56596bdcbbe9bee023bd8cbf98
403-2 <cthtufgbv@mail.ru>
2018-12-24 11:35:56 +0600

    add func

--- database.py
+++ database.py
@@ -7,7 +7,6 @@ from queryfactory import QueryFactory
 
 
 class DatabaseInspector:
-
     host = ""
     username = ""
     password = ""
@@ -131,51 +130,58 @@ class DatabaseInspector:
             return "database not connection"
         return self.execute(query_object)[0][0]
 
-    def add_book(self, name_book="", name_genre="", author="",
-                        isbn="", description="", number_of_pages_book=0,
-                            release_date=0, book_binding_type="", index_udc=0, index_bbk=0):
+    def add_book(self, authors, name_genre, name_book="",
+                 isbn="", description="", number_of_pages_book=0,
+                 release_date=0, book_binding_type="", index_udc=0, index_bbk=0):
 
-            if self.db is None:
-                return "database not connection"
+        if self.db is None:
+            return "database not connection"
 
-            # prepare a cursor object using cursor() method
-            cursor = self.db.cursor()
+        # prepare a cursor object using cursor() method
+        cursor = self.db.cursor()
 
-            index_book_binding_type = self.what_is_index(QueryFactory.search_by_binding_book(book_binding_type))
+        index_book_binding_type = self.what_is_index(QueryFactory.search_by_binding_book(book_binding_type))
 
-            # Добавление книги
-            self.execute(QueryFactory.add_row_in_table_books(name_book = name_book,
-                                                             number_of_pages_book = number_of_pages_book,
-                                                             index_book_binding_type = index_book_binding_type,
-                                                             release_date_book = release_date,
-                                                             index_udc = index_udc,
-                                                             index_bbk = index_bbk,
-                                                             isbn = isbn))
+        # Добавление книги
+        self.execute(QueryFactory.add_row_in_table_books(name_book=name_book,
+                                                         number_of_pages_book=number_of_pages_book,
+                                                         index_book_binding_type=index_book_binding_type,
+                                                         release_date_book=release_date,
+                                                         index_udc=index_udc,
+                                                         index_bbk=index_bbk,
+                                                         isbn=isbn))
 
-            # Добавление авторов
-            self.execute(QueryFactory.add_row_in_table_author(author))
+        # Добавление авторов
+        for author_book in authors:
+            self.execute(QueryFactory.add_row_in_table_author(author_book))
 
-            # Взять индекс книги по названию
-            index_book = self.what_is_index(QueryFactory.search_by_name_book(name_book))
+        # Взять индекс книги по названию
+        index_book = self.what_is_index(QueryFactory.search_by_name_book(name_book))
 
-            # Взять индекс автора по имени
-            index_author = self.what_is_index(QueryFactory.search_by_name_author(author))
+        # Взять индекс автора по имени
+        index_author = []
+        for author_book in authors:
+            index_author.append(self.what_is_index(QueryFactory.search_by_name_author(author_book)))
 
-            # Cвязать авторов с книгой
-            self.execute(QueryFactory.add_row_in_author_join_table(index_book, index_author))
+        # Cвязать авторов с книгой
+        for author_book in index_author:
+            self.execute(QueryFactory.add_row_in_author_join_table(index_book, author_book))
 
-            # Добавление жанр
-            self.execute(QueryFactory.add_row_in_table_genre(name_genre))
+        # Добавление жанр
+        for genre in name_genre:
+            self.execute(QueryFactory.add_row_in_table_genre(genre))
 
-            # Взять индекс жанра по названию
-            index_genre = self.what_is_index(QueryFactory.search_by_binding_book(book_binding_type))
+        # Взять индекс жанра по названию
+        index_genre = []
+        for genre in name_genre:
+            index_genre.append(self.what_is_index(QueryFactory.search_by_name_genre(genre)))
 
-            # Cвязать жанра с книгой
-            self.execute(QueryFactory.add_row_in_genre_join_table(index_book, index_genre))
+        # Cвязать жанра с книгой
+        for genre in index_genre:
+            self.execute(QueryFactory.add_row_in_genre_join_table(index_book, genre))
 
-            # Добавить описание книге
-            self.execute(QueryFactory.add_row_in_table_description(description))
-
+        # Добавить описание книге
+        self.execute(QueryFactory.add_row_in_table_description(description))
 
 
 if __name__ == '__main__':
@@ -183,8 +189,8 @@ if __name__ == '__main__':
     db.connect('127.0.0.1', 'hays0503', 'hays0503', 'librarydb')
 
     name_book = 'Хуй'
-    name_genre = 'Хуйня внеземная'
-    author = 'Пизда Ивановна'
+    name_genre = ['Хуйня внеземная']
+    author = ['Пизда Ивановна', 'Владимир серый хуй']
     description = "description description description description"
     isbn = "12345"
     index_bbk = 1
@@ -192,13 +198,13 @@ if __name__ == '__main__':
     number_of_pages_book = 200
     release_date = 2007
     book_binding_type = "мягкая обложка"
-    db.add_book(name_book = name_book,
-                name_genre = name_genre,
-                author=author,
-                isbn = isbn,
-                index_udc = index_udc,
-                index_bbk = index_bbk,
-                description = description,
-                number_of_pages_book = number_of_pages_book,
-                release_date = release_date,
-                book_binding_type = book_binding_type)
+    db.add_book(name_book=name_book,
+                name_genre=name_genre,
+                authors=author,
+                isbn=isbn,
+                index_udc=index_udc,
+                index_bbk=index_bbk,
+                description=description,
+                number_of_pages_book=number_of_pages_book,
+                release_date=release_date,
+                book_binding_type=book_binding_type)
